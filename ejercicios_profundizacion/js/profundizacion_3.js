b1553c1a516c364503d8cde96274f2ebd87032dd
velaquia <32940917+velaquia@users.noreply.github.com>
2023-08-23 14:04:04 -0300

    Update profundizacion_3.js

--- ejercicios_profundizacion/js/profundizacion_3.js
+++ ejercicios_profundizacion/js/profundizacion_3.js
@@ -20,14 +20,14 @@
 // 2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
 // Para este caso haga uso de la herramienta "Math.min",
 // investigue y busque como utilizarla.
-let temp1 = parseInt(prompt("Ingrese la primer temperatura"))
-let temp2 = parseInt(prompt("Ingrese la segunda temperatura"))
-let temp3 = parseInt(prompt("Ingrese la tercer temperatura"))
+let temp1 = parseInt(prompt("Ingrese la primer temperatura"));
+let temp2 = parseInt(prompt("Ingrese la segunda temperatura"));
+let temp3 = parseInt(prompt("Ingrese la tercer temperatura"));
 
-let temp_maxima = Math.max(temp1, temp2, temp3)
-console.log("La temperatura maxima es:", temp_maxima)
+let temp_maxima = Math.max(temp1, temp2, temp3);
+console.log("La temperatura maxima es:", temp_maxima);
 
-let temp_minima = Math.min(temp1, temp2, temp3)
-console.log("La temperatura minima es:", temp_minima)
+let temp_minima = Math.min(temp1, temp2, temp3);
+console.log("La temperatura minima es:", temp_minima);
 
 // En cada caso imprimir en pantalla el resultado
