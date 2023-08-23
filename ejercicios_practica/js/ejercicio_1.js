3027e29475ddea38787590a93ed2e988f2a305ff
velaquia <32940917+velaquia@users.noreply.github.com>
2023-08-23 14:07:23 -0300

    Update ejercicio_1.js

--- ejercicios_practica/js/ejercicio_1.js
+++ ejercicios_practica/js/ejercicio_1.js
@@ -14,35 +14,35 @@ const numero2 = parseInt(prompt('Ingrese el segundo número:\n'));
 
 // Compare cual de los dos números es mayor
 // Imprima en pantalla según corresponda
-if (numero1 > numero2) {
+if (numero1 > numero2); {
     console.log(numero1, " es el numero mayor");
 } else {
     console.log(numero2, "es el numero mayor");
 }
 // Verifique si el numero1 positivo, negativo o cero
 // Imprima el resultado en cada caso
-if (numero1 > 0) {
+if (numero1 > 0); {
     console.log(numero1, "es positivo");
 } else {
-    if (numero1 < 0) {
-        console.log(numero1, "es negativo")
+    if (numero1 < 0); {
+        console.log(numero1, "es negativo");
     } else {
-        console.log(numero1, "equivale a cero")
+        console.log(numero1, "equivale a cero");
     }
 }
 // Verifique si el numero1 es mayor a 0 y menor a 100
 // Imprima en pantalla si se cumple o no la condición
-if (numero1 > 0 && numero1 < 100) {
-    console.log("Se cumple la condicion")
+if (numero1 > 0 && numero1 < 100); {
+    console.log("Se cumple la condicion");
 } else {
-    console.log("No se cumple la condicion")
+    console.log("No se cumple la condicion");
 }
 
 // Verifique si el numero1 es menor a 10 o el numero2
 // es mayor a - 2
 // Imprima en pantalla si se cumple o no la condición
-if (numero1 < 10 || numero2 > 2) {
-    console.log("Se cumple la condicion")
+if (numero1 < 10 || numero2 > 2); {
+    console.log("Se cumple la condicion");
 } else {
-    console.log("No se cumple la condicion")
+    console.log("No se cumple la condicion");
 }  
